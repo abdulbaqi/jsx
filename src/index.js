89e4f008c361e52079cadf3c21b650035699eeb0
abdulbaqi <sharaf.al.islam@gmail.com>
2019-04-18 10:45:44 +0300

    first

---
+++ src/index.js
@@ -0,0 +1,38 @@
+//import lib
+import React from "react";
+import ReactDOM from "react-dom";
+
+//create react component
+
+//we can use functional components
+
+const getValue = () => {
+  return "enter your name";
+};
+
+const getTime = () => {
+  return new Date().toLocaleTimeString();
+};
+
+const App = () => {
+  const buttonText = "Click Me";
+  const secondButton = { text: "I am 2" };
+  const styleButton2 = { backgroundColor: "red", color: "yellow" };
+
+  return (
+    <div>
+      <label className="label" htmlFor="name">
+        Enter Name:
+      </label>
+      <input id="name" value={getValue()} />
+      <button style={{ backgroundColor: "blue", color: "white" }}>
+        {buttonText}
+      </button>
+      <button style={styleButton2}>{secondButton.text}</button>
+      <p>The time now is {getTime()}</p>
+    </div>
+  );
+};
+
+//show on screen
+ReactDOM.render(<App />, document.querySelector("#root"));
